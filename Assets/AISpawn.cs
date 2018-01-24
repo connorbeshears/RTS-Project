6a07209e04525e1a4f183471d5c137e92d03e239
Connor Beshears <connorbeshears@gmail.com>
2018-01-23 23:05:29 -0600

    Units
    Added two peasant units and scripts so automatically spawn them.
    TODO: Change spawn to calculated probablity

---
+++ Assets/AISpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISpawn : MonoBehaviour {
+    public GameObject Spawn;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("spawnUnit", 3.0f, 3.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void spawnUnit()
+    {
+        Vector3 spawnPos = transform.position;
+        Instantiate(Spawn, spawnPos, Quaternion.identity);
+    }
+}
---
+++ Assets/GreenMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenMove : MonoBehaviour {
+    GameObject friend;
+    GameObject enemy;
+    public double health = 100;
+    bool hasTarget;
+    public float move = 0.4f;
+    Vector3 enemyLocation;
+    // Use this for initialization
+    void Start()
+    {
+        enemy = GameObject.FindGameObjectWithTag("RedUnit");
+        hasTarget = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemy != null)
+        {
+            enemyLocation = new Vector3(enemy.transform.position.x, enemy.transform.position.y, enemy.transform.position.z);
+            if (Vector3.Distance(enemyLocation, transform.position) > 0.5f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, enemyLocation, move * Time.deltaTime);
+            }
+
+
+            if (Vector3.Distance(enemyLocation, transform.position) < 0.7f)
+            {
+                health -= 2;
+            }
+        }
+
+        if (health < 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if(enemy == null)
+        {
+            enemy = GameObject.FindGameObjectWithTag("RedUnit");
+        }
+    }
+}
