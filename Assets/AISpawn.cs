5a572f8c1ba08b5502f2e6deebb8d764decccc25
Connor Beshears <connorbeshears@gmail.com>
2018-01-24 13:32:31 -0600

    Spawn Button and Unit Chance
    Fixed AI spawn to be chance based and changed player spawn to force a button press
    TODO: Battle System (i.e. one unit can't kill 100)

--- Assets/AISpawn.cs
+++ Assets/AISpawn.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class AISpawn : MonoBehaviour {
     public GameObject Spawn;
+    public float chance = .1f;
+    GameObject[] enemies;
+    int numEnemies = 0;
+    float buff;
+    float randNum;
 	// Use this for initialization
 	void Start () {
-        InvokeRepeating("spawnUnit", 3.0f, 3.0f);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
+        enemies = GameObject.FindGameObjectsWithTag("RedUnit");
+        numEnemies = enemies.Length;
+
+        chance = numEnemies / 1000f;
+
+        randNum = Random.Range(0.0f, 1.0f);
+
+        if(randNum < chance)
+        {
+            spawnUnit();
+        }
+
+    
+
 	}
 
     void spawnUnit()
---
+++ Assets/ButtonSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSpawn : MonoBehaviour {
+    public GameObject unit;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void taskOnClick()
+    {
+        Debug.Log("Ran Script");
+        GameObject spawnPoint = GameObject.FindGameObjectWithTag("RedBuilding");
+        Vector3 spawnPos = spawnPoint.transform.position;
+        Instantiate(unit, spawnPos, Quaternion.identity);
+    }
+}
